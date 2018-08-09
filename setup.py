43ee7d3c6c078002bb2c00ed85ef22aaa008e0bc
devongoetz <35565233+devongoetz@users.noreply.github.com>
2018-08-09 16:03:52 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ballflight',
+      version='0.1dev',
+      description='A package that allows you to model the flight of a ball from either a pitch or a swing.',
+      author='Devon Goetz',
+      license='MIT',
+      package=['ballflight',],
+      long_description=open('README.txt').read())
